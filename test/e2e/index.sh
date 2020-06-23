5d0103b5a0139ab77e8bff107bed62741918da24
yemiancheng <hualei03042013@163.com>
2020-06-23 08:08:08 +0800

    test(core): init test

---
+++ test/e2e/index.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+THIS_FILE_PATH=$(cd $(dirname $0);pwd)
+THIS_FILE_NAME=$(basename $0)
+[ -z "$RUN_SCRIPT_PATH" ] && RUN_SCRIPT_PATH=$(pwd)
+
+PROJECT_PATH=$(cd "$THIS_FILE_PATH";cd "../../" ;pwd)
+
+# import some lib
+source "${PROJECT_PATH}/sh_modules/sh-lib-project-dir-map.sh"
+# project_dir_map_gen2 "../"
+[ -z "$THIS_FILE_PATH" ] && THIS_FILE_PATH=$(this_file_path_get) ; project_dir_map_gen2 "../"
+
+echo "todos"
+
+for key in $(echo ${!db1e4d[*]}) ;do val=${db1e4d[$key]}; if [ -e $key ] ; then . $key ; else echo "${key} does not exsits." ; fi ; done
+
+# file usage
+# test/e2e/index.sh
